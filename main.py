57a5b7ad03a6596f6fd4537d2885a71bea25afa2
izanami <quanticdemon@gmail.com>
2026-05-30 06:36:13 +0600

    accounts logic its almost complete

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import *
 import uuid
 import hashlib
 import sqlite3
+import os
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
-app.secret_key = "ae6d651fa84a5ccc16a6574601db6dca/@*axmsao129n29330231j2091n39192s901292192012js9j102js019"
+app.secret_key = "ae6dasasdsddd6514fa8d4a5ccc16a6574601db6dca/@*axmsao129n29330231j2091n3s9e313fs92s901292192012js9j102js019"
 #DB 
 
 class Storage:
@@ -18,6 +20,9 @@ class Storage:
     def update_data(self, datausr_package):
         pass
     def consult_data(self, datausr_package):
+        pass
+    
+    def update_profile_pic(self,image):
         pass
 class SQLite3(Storage):
     def conexion(self):
@@ -36,6 +41,19 @@ class SQLite3(Storage):
                     )
                 '''
 
+        )
+
+        cursor.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS profile_pic(
+                id_image TEXT PRIMARY KEY,
+                image_profile TEXT NOT NULL,
+                id_user TEXT,
+                FOREIGN KEY (id_user) REFERENCES users(id)
+
+
+             )   
+            '''
         )
         conn.commit()
         cursor.close()
@@ -92,6 +110,72 @@ class SQLite3(Storage):
             }
         else:
             return False
+    def update_data(self, datausr_package):
+        conn = self.conexion()
+        cursor = conn.cursor()
+        
+        fields_tochange = []
+        values = []
+
+        if datausr_package['username']:
+            fields_tochange.append("username = ?")
+            values.append(datausr_package['username'])
+        
+        if datausr_package['password']:
+            fields_tochange.append("password = ?")
+            values.append(datausr_package['password'])
+        
+        if datausr_package['user_mail']:
+            fields_tochange.append("mail = ?")
+            values.append(datausr_package['user_mail'])
+
+        
+        query = f"""
+        UPDATE users
+        SET {', '.join(fields_tochange)}
+        WHERE id = ?
+        """
+
+        values.append(session.get('user_id'))
+        cursor.execute(query, values)
+
+
+
+           
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    def update_profile_pic(self, image):
+        conn = self.conexion()
+        cursor = conn.cursor()
+
+        image_id=str(uuid.uuid4())
+        cursor.execute(
+            """
+            INSERT INTO profile_pic(id_image, image_profile ,id_user) VALUES (?,?,?)
+            """,(
+                image_id,
+                image,
+                session['user_id']
+            )
+        )
+
+       
+            
+            
+        image_upload = cursor.fetchone()
+
+
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return {
+        "id_image": image_id,
+        "image_profile": image,
+        "id_user": session['user_id']
+    }
 class Storage_Service:
     def __init__(self, storage:Storage):
         self.storage = storage
@@ -100,6 +184,10 @@ class Storage_Service:
         return self.storage.save_data(datausr_package)
     def consult_data_service(self, datausr_package):
         return self.storage.consult_data(datausr_package)
+    def update_data_service(self, datausr_package):
+        return self.storage.update_data(datausr_package)
+    def update_profile_pic_service(self, image):
+        return self.storage.update_profile_pic(image)
 #Users
 class User:
     def create_account(self, name, mail, password):
@@ -122,26 +210,69 @@ class User:
 
     def sing_in(self, loginUser, password):
         hashPass = hashlib.sha512(password.encode())
-        password = hashPass.hexdigest()
+        hashedPass = hashPass.hexdigest()
 
         userData_package ={
             "userToken":loginUser,
-            "password":password
+            "password":hashedPass
         }
+       
 
         sql3 = SQLite3("kitTest.db")
         sql3.start_tables()
         serviceStorage = Storage_Service(sql3)
         return serviceStorage.consult_data_service(userData_package)
+    def update(self, changes):
+        passwordHashing = hashlib.sha512(changes['password'].encode())
+        passwordUpdate = passwordHashing.hexdigest()
 
+        user_package_tochange ={
+            "username":changes['username'],
+            "user_mail":changes['mail'],
+            "password":passwordUpdate
+
+        }
+
+        sqlite3 = SQLite3("kitTest.db")
+        sqlite3.start_tables()
+        serviceStorage = Storage_Service(sqlite3)
+        serviceStorage.update_data_service(user_package_tochange)
+    @staticmethod
+    def messenger_images(image):
+        sqlite3 = SQLite3("kitTest.db")
+        sqlite3.start_tables()
+        storageService = Storage_Service(sqlite3)
+        return storageService.update_profile_pic_service(image)
 
 #context_procesor
 @app.context_processor
 def inject_user_data():
+    userpic = None
+
+    if session.get('user_id'):
+            conn = SQLite3("kitTest.db").conexion()
+            cursor = conn.cursor()
+
+            cursor.execute(
+                "SELECT image_profile FROM profile_pic WHERE id_user = ? ORDER BY id_image DESC LIMIT 1",
+                (session['user_id'],)
+            )
+
+            row = cursor.fetchone()
+
+            if row:
+                userpic = url_for('uploads_filename', filename=row[0])
+
+            cursor.close()
+            conn.close()
+
+
     return {
         'username':session.get('username'),
         'mail':session.get('user_mail'),
-        'is_logged_in':'user_id' in session
+        'is_logged_in':'user_id' in session,
+        'userpic':userpic
+
     }
 
 #Flask
@@ -159,9 +290,11 @@ def home():
     return render_template("home.html")
 @app.route('/create-account', methods=["GET", "POST"])
 def register():
+    session.clear()
     username = request.form.get("username")
     mail = request.form.get("mail")
     password = request.form.get("password")
+    
     action = request.form.get("action")
     if request.method == "POST":
         if action == "Confirm":
@@ -172,6 +305,7 @@ def register():
 
 @app.route('/sign-in', methods=["GET", "POST"])
 def login():
+    session.clear()
     userToken = request.form.get('loginUser')
     password = request.form.get('password')
     action = request.form.get('action')
@@ -185,6 +319,10 @@ def login():
             session['user_id'] = verification['id']
             session['username'] = verification['username']
             session['user_mail'] = verification['mail']
+            session['password'] = verification['password']
+           
+
+    
             return redirect(url_for("home"))
         else:
             return render_template("sign-in.html", verification = verification)
@@ -193,16 +331,67 @@ def login():
 
 @app.route('/my-profile', methods=["GET","POST"])
 def user_profile():
+
+    print("METHOD", request.method)
+    print("FILES:", request.files)
     if 'user_id' not in session:
         return redirect(url_for("login"))
 
+    username = request.form.get("username")
+    mail = request.form.get("mail")
+    password = request.form.get("password")
+    action = request.form.get("action")
+    
+
+    
+    if request.method == "POST" and action == "update":
+        file = request.files["changeImage"]
+        if file and file.filename:
+            valid_extension = ['png', 'jpg', 'jpeg', 'webp']
+            extension = file.filename.rsplit('.', 1)[1].lower()
+            print(request.files)
+            print(request.files.keys())
+            if extension in valid_extension:
+                filename = f"{str(uuid.uuid4())}.{extension}"
+                print(os.getcwd())
+                print(os.path.abspath("uploads"))
+                print(os.path.exists("uploads"))
+                file.save(f"uploads/{filename}")
+                updateImg = User().messenger_images(filename)
+
+
+                
+
+            
+        changes = {
+            "username": username if username else session.get("username"),
+            "mail": mail if mail else session.get("user_mail"),
+            "password": password if password else session.get("password")
+
+        }
+    
+        update_user = User()
+        update_user.update(changes)
+        session['username'] = changes['username']
+        session['user_mail'] = changes["mail"]
+        session['password'] = changes['password']
+       
     return render_template('user-profile.html')
+@app.route('/uploads/<filename>')
+def uploads_filename(filename):
+    return send_from_directory('uploads', filename)
 
 @app.route('/my-projects', methods=["GET","POST"])
 
 def my_projects():
     return render_template("my-projects.html")
 
+@app.route('/productivity-tools', methods=["GET","POST"])
+
+def productivity_tools():
+    return render_template("productivity.html")
 
 if __name__ == "__main__":
+    
     app.run(debug=True)
+
